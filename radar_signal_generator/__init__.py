742023ccd2e2f20f9a6a76bcf6850ea5a6af8821
Veikko Markkula <veikko.markkula@aalto.fi>
2025-09-11 17:14:03 +0300

    Simple working signal generators for pulse and chirp added

---
+++ radar_signal_generator/__init__.py
@@ -0,0 +1,199 @@
+"""
+================
+Signal Generator
+================
+
+Multi-purpose signal generator for The SyDeKick. Can generate sine waves and
+pulse-shape signals.
+
+"""
+
+import os
+import sys
+if not (os.path.abspath('../../thesdk') in sys.path):
+    sys.path.append(os.path.abspath('../../thesdk'))
+
+import numpy as np
+import scipy.signal as scsig
+import tempfile
+import pdb
+
+from dataclasses import dataclass
+
+from thesdk import *
+from vhdl import *
+
+@dataclass
+class rect_attrib:
+    T       : float      # Pulse width [s]      
+    t_start : float      # Pulse start time [s]
+    N       : int        # Numbe of samples
+    fs      : float      # Sample rate [smaples/s]
+
+@dataclass
+class chirp_attrib:
+    T       : float      # Pulse width [s]
+    B       : float      # Sweep bandwidth [s]
+    N       : int        # Number of samples [samples/s]
+
+class radar_signal_generator(thesdk):
+    """
+    Attributes:
+    TODO
+
+    """
+
+    @property
+    def _classfile(self):
+        return os.path.dirname(os.path.realpath(__file__)) + "/"+__name__
+
+    def __init__(self,*arg): 
+        self.print_log(type='I', msg='Initializing %s' %(__name__)) 
+        self.proplist = ['sig_freq','lo_freq','sig_amp','sig_cm','coherent','tau','sig_phase','sig_osr','nsamp','fs','extra_sampl', 'nonoverlap_period']
+        self.sig_freq = 1e6
+        self.lo_freq = 0
+        self.sig_amp = 0.5
+        self.tau = 0
+        self.sig_cm = 0
+        self.sig_phase = 0
+        self.sig_osr = 1
+        self.nsamp = 1024
+        self.extra_sampl = 0
+        self.fs = 2e9
+        self.snr = 0
+        self.jitter_sd = None
+        self.coherent = False
+        self.sigtype = 'sine'
+        self.high = 1
+        self.low = 0
+        self.after = 0
+        self.duty = 0.5
+        self.trise = 5e-12
+        self.tfall = 5e-12
+        self.nonoverlap_period = 40e-12
+        self.slopetype='rising'
+        self.IOS=Bundle()
+        self.IOS.Members['out']= IO()
+        self.model='py'
+        self.init()
+
+        if len(arg)>=1:
+            parent=arg[0]
+            self.copy_propval(parent,self.proplist)
+            self.parent =parent;
+
+    def init(self):
+        pass
+
+    def run(self,*arg):
+        if self.model=='py':
+            return self.main()
+
+        else: 
+            pass
+
+    def main(self):
+        """
+        Generates chosen signal type and returns it as "ndarray[np.complex128]" type.
+        """
+
+        # Signal
+        signal_type = 'chirp'
+
+        # TEMPORARY PARAMETER DEFINITIONS (replace with self.config)
+        rect_config     = rect_attrib(T=1e-3, t_start=1e-4, N=1000, fs=1e6)
+        chirp_config    = chirp_attrib(T=1e-3, B=10, N=1024)
+                
+        match signal_type: 
+            case 'rect':
+                return self.rect(rect_config)
+            case 'chirp':
+                return self.chirp(chirp_config)
+            # TODO: case 'binary_coded_PSK': 
+            case _:
+                self.print_log(type='F',msg='Signal type \'%s\' not supported.' % self.signal_type)
+                return None
+
+
+    def rect(self, config: rect_attrib):
+        """
+        Parameters:
+        rect_config(T, t_start, N, fs)  # Rect signal attributes
+        ----------
+        Returns:
+        x : ndarray(dtype=complex128)   # Generated pulse waveform I/Q samples
+
+        """
+        T, t_start, N, fs = config.T, config.t_start, config.N, config.fs
+        fs, N = self.fs, self.nsamp
+
+        n_high = int(T*fs)
+        n_start = int(t_start*fs)
+        n_end = min(N, n_start + n_high)
+        x = np.zeros(N, dtype=np.complex128)
+        x[n_start:n_end] = np.ones(n_end-n_start, dtype=np.complex128)
+
+        return x
+
+    def chirp(self, config: chirp_attrib):
+        """
+        Parameters:
+        chirp_config(T, B, N)  # Rect signal attributes
+        ----------
+        Returns:
+        x : ndarray(dtype=complex128)   # Generated pulse waveform I/Q samples
+        """
+        import scipy
+
+        #T, B, N = config.T, config.B, config.N
+        #fs, Nr = self.fs, self.nsamp
+
+        N, T = 1000, 0.01
+        t = np.arange(N) * T
+        chirp_pulse = scipy.signal.chirp(t, f0=6, f1=1, t1=10, method='linear', complex=True)
+
+        ## Derived parameters
+        #n_end = int(T*fs)
+        #t = np.arange(n_end)/fs
+        #f1 = -B/2
+        #f2 = B/2
+
+        ## Generate chirp I/Q samples
+        #chirp_pulse = np.zeros(Nr, dtype=np.complex128)
+        #chirp_pulse[:n_end].real = scipy.signal.chirp(t, f1, T, f2, method='linear', phi=90)
+        #chirp_pulse[:n_end].imag = scipy.signal.chirp(t, f1, T, f2, method='linear', phi=0)
+
+        return chirp_pulse
+
+
+if __name__=="__main__":
+    from  radar_signal_generator import *
+    from  iq_plotting import *
+    import pdb
+    import matplotlib.pyplot as plt
+
+    dut = radar_signal_generator()
+    dut.model = 'py'
+    x = dut.run()
+
+    n = np.arange(2**20)
+    #fs = 2**20
+    fs = dut.fs
+    #x = np.concatenate((np.ones(10), np.zeros(len(n)-10))) + 0j*np.zeros(len(n)) # Working test signal
+    x_scaled = scale_dbm(x, -30)
+    # time-domain
+    plt.figure()
+    plt.plot(x_scaled.real[:64], 'blue')
+    plt.plot(x_scaled.imag[:64], 'red')
+    plt.legend(['I','Q'])
+    # spectrum
+    plt.figure()
+    plt.grid()
+    plt.ylabel("dBm")
+    plot_bb_spectrum(x_scaled,fs,scale='v2dbm',window='rect', color='black', ylim=[-70,10])
+    #plot_bb_spectrum(x_scaled,fs,scale='dbfs',window='rect', color='black', ylim=[-70,10])
+
+    # Show plot
+    plt.show()
+    
+    input()
---
+++ radar_signal_generator/iq_plotting.py
@@ -0,0 +1,131 @@
+import scipy
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+# set sensible default values for matplotlib style
+matplotlib.rcParams['lines.linewidth'] = 2
+matplotlib.rcParams['xtick.labelsize'] = 8
+matplotlib.rcParams['ytick.labelsize'] = 8
+matplotlib.rcParams['figure.figsize'] = (3.5, 2.2)
+matplotlib.rcParams['figure.dpi'] = 150
+matplotlib.rcParams['font.size'] = 8
+matplotlib.rcParams['figure.autolayout'] = True
+
+def plot_bb_spectrum(x, fs, scale='dbfs', annotate_max=False, window='rect', color='red', ylim=[-100,10]):
+  N = len(x)
+
+  # Apply windowing function
+  # use sym=False for spectral analysis
+  if window == 'rect':
+    window = np.ones(N)
+  elif window == 'flattop':
+    # for power estimation
+    window = scipy.signal.windows.flattop(N, sym=False)
+  elif window == 'hann':
+    window = scipy.signal.windows.hann(N, sym=False)
+  else:
+    raise NotImplementedError("window type not implemented")
+  # normalize by average value of windowing function to preserve amplitude
+  window /= np.mean(window)
+  x_windowed = x*window
+
+  # normalization of X is not strictly necessary if plotting in dBFS
+  f = scipy.fft.fftfreq(N, d=1/fs)
+  X = scipy.fft.fft(x_windowed) / N
+  f2 = scipy.fft.fftshift(f)
+  X2 = scipy.fft.fftshift(X)
+
+  ax = plt.gca()
+
+  # amplitude to dB conversion
+  if scale=='abs':
+    P_plot = np.abs(X2)
+  if scale=='dbfs':
+    P = np.abs(X2)**2
+    P_norm = P / np.max(P)
+    P_plot = 10*np.log10(P_norm)
+    ax.set_ylim(ylim[0], ylim[1])
+  if scale=='v2dbm':
+    # amplitude to dbm, assuming np.abs(X2) is voltage (amplitude)
+    voltage = np.abs(X2)
+    power = (voltage**2)/(2*50)
+    P_dbm = 10*np.log10(power/0.001)
+    P_plot = P_dbm
+    ax.set_ylim(ylim[0], ylim[1])
+
+
+  # formatter = EngFormatter(unit='Hz')
+  # ax.xaxis.set_major_formatter(formatter)
+
+  # adjust freq unit
+  ax.set_xlabel("Frequency [MHz]")
+  f2 /= 1e6
+
+  # zoom in
+  # todo: parameterize
+  width = 256
+  start = N//2 - width
+  end = N//2 + width
+
+  # don't zoom in
+  start = 0
+  end = len(x)
+
+  # find peak
+  if annotate_max:
+    i_max = np.argmax(P_plot)
+    P_max = P_plot[i_max]
+
+  # plot spectrum
+  plt.plot(f2[start:end], P_plot[start:end], linewidth=2, color=color)
+
+  # annotate peak
+  if annotate_max:
+    plt.scatter(f2[i_max], P_max)
+    plt.annotate(f"({f2[i_max]:.3f}, {P_max:.1f})", xy=(f2[i_max],P_max), xycoords='data', xytext=(5,5), textcoords='offset points', fontsize=8)
+    print(f"peak at f={f2[i_max]}")
+
+  return (f2, X2)
+
+def scale_dbm(x, dbm):
+  """Scale signal amplitude so that its peak power has `dbm` power"""
+  P_abs = 0.001 * 10**(dbm/10)
+
+  # measure peak power of original signal
+  N = len(x)
+  # no need to window because we are analyzing periodic signals with transients
+  X = scipy.fft.fft(x) / N
+  P = (np.abs(X)**2)/(2*50)
+  P_max = np.max(P)
+
+  # required gain
+  gain_abs = P_abs / P_max
+  gain_ampl = np.sqrt(gain_abs)
+
+  return x * gain_ampl
+
+if __name__ == "__main__":
+  # test scale_dbm
+  fs = 2**20
+  n = np.arange(2**20)
+  # 0dBm sine wave
+  # x = 0.224*np.sqrt(2)*np.exp(2j*np.pi*200e3*n/fs)
+  x = np.concatenate((np.ones(10), np.zeros(len(n)-10))) + 0j*np.zeros(len(n))
+  # rect pulse
+  x_scaled = x
+  x_scaled = scale_dbm(x, -30)
+  # time-domain
+  plt.figure()
+  plt.plot(x_scaled.real[:64], 'blue')
+  plt.plot(x_scaled.imag[:64], 'red')
+  plt.legend(['I','Q'])
+  # spectrum
+  plt.figure()
+  plt.grid()
+  plt.ylabel("dBm")
+  plot_bb_spectrum(x_scaled,fs,scale='v2dbm',window='rect', color='black', ylim=[-70,10])
+  #plot_bb_spectrum(x_scaled,fs,scale='dbfs',window='rect', color='black', ylim=[-70,10])
+
+  # Show plot
+  plt.show()
